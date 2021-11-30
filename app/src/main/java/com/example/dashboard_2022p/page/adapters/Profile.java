ee3f7570d6524a7548c0301c15d1ddc97774c911
EricLiIIT <eli7@hawk.iit.edu>
2021-11-29 22:25:43 -0600

    fixed profile gui

--- app/src/main/java/com/example/dashboard_2022p/page/adapters/Profile.java
+++ app/src/main/java/com/example/dashboard_2022p/page/adapters/Profile.java
@@ -1,11 +1,31 @@
 package com.example.dashboard_2022p.page.adapters;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.dashboard_2022p.R;
 
 public class Profile extends com.example.dashboard_2022p.page.Page {
+
+    private ViewGroup rootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @
+            Nullable Bundle SavedInstanceState) {
+        rootView = (ViewGroup) inflater.inflate(R.layout.profile, container, false);
+        return rootView;
+    }
+
     @NonNull
     @Override
     public String getTitle() {
         return "Profile";
     }
+
 }
